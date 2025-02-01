6e5d411ba736bc6d2ffcce9000adf775faa651f3
KOTFAN <kotfan.empire@gmail.com>
2025-02-01 15:57:19 +0200

    add Intellecta progect v alpha 1.0

---
+++ src/js/files/script.js
@@ -0,0 +1,14 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+
+// document.querySelector('.hero__scroll-down').addEventListener('click', function () {
+//    const targetElement = document.querySelector('.article-content');
+//    if (targetElement) {
+//       targetElement.scrollIntoView({ behavior: 'smooth' });
+//    } else {
+//       console.error('Element with class .article-content not found.');
+//    }
+// });
